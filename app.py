7300e3205bbb54926e4be593d2123d174a953d67
clesiomatias <clesiofmatias@gmail.com>
2023-05-09 21:09:39 -0300

    add cors

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+CORS(app)
 
 # Configuração do banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///portal_animes.db'
